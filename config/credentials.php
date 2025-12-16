93fee7df0affb3ae87ebb267fe11a4657c3ffe7d
Thanh Thuyet <lethuyet6904@gmail.com>
2025-12-16 18:02:45 +0700

    fix bug

--- config/credentials.php
+++ config/credentials.php
@@ -1,11 +1,7 @@
 <?php
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-<<<<<<< HEAD
 define('DB_PASSWORD', ''); 
-=======
-define('DB_PASSWORD', '');	
->>>>>>> 0168500f6aa6b1c4dcb55b8d15e6084536e6a0d1
 define('DB_NAME', 'mybhotel_db');
 
 ?>
