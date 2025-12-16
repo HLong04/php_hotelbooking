27798efc0b3249d7bee60cf6d8e32aecd28e1d43
dinhphu-zin <truongph815@gmail.com>
2025-12-16 17:17:46 +0700

    Sua layour

--- templates/layouthome.php
+++ templates/layouthome.php
@@ -16,7 +16,7 @@
                 <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <circle cx="20" cy="20" r="18" stroke="#e74c3c" stroke-width="2" fill="none" />
                 </svg>
-                <span>Bistro Elegance</span>
+                <span>Bistro</span>
             </a>
             <ul class="nav-links">
                 <li><a href="#home" class="active">Home52236</a></li>
