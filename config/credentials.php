61e751e08dd4acaeb4dc7e8484c00ce0fe90122e
Thanh Thuyet <lethuyet6904@gmail.com>
2025-12-22 17:56:15 +0700

    fix login

--- config/credentials.php
+++ config/credentials.php
@@ -3,7 +3,7 @@
 // Kiểm tra: Nếu chưa có DB_HOST thì mới định nghĩa
     define('DB_HOST', 'localhost'); 
     define('DB_USER', 'root');
-    define('DB_PASSWORD', 'admin'); // Điền pass nếu có, không thì để trống
+    define('DB_PASSWORD', ''); // Điền pass nếu có, không thì để trống
     define('DB_NAME', 'mybhotel_db');
     
 ?>
